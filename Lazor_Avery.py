3b7728ca590a0e13f1c925911ffe632a17e22203
AveryTran <47228565+AveryTran@users.noreply.github.com>
2019-04-10 13:43:21 -0400

    Update Lazor_Avery.py
    Proper grid data pull that filters out whitespace

--- Lazor_Avery.py
+++ Lazor_Avery.py
@@ -4,19 +4,26 @@
 
 def data(filename):
 	fptr = open(filename, 'r').read()
-	grid = fptr.split('\n')[4:7]
+	all_lines = fptr.split('\n')
+	raw_data = []
+	for line in all_lines:
+		if '#' not in line and line != '':
+			raw_data.append(line)
+
 	setup = fptr.split('\n')[9:]
-	# block = []
-	# for x in setup[:3]:
-	# 	if x[0] == 'A':
-	# 		A = int(x[2])
-	# 	elif x[0] == 'B':
-	# 		B = int(x[2])
-	# 	elif x[0] == 'C':
-	# 		C = int(x[2])
-	return grid, setup
+	grid_start = raw_data.index('GRID START')
+	grid_stop = raw_data.index('GRID STOP')
+
+	grid_raw = raw_data[grid_start+1:grid_stop]
+	grid = []
+	for g in grid_raw:
+		x = ''.join(g.split())
+		grid.append(x)
+	setup[:] = (line for line in setup if line != '')
+
+	return grid
 
 if __name__ == "__main__":
-	grid, setup = data('dark_1.bff')
-	# print(B)
-	print(grid, '\n', setup, sep = '')
+	grid = data('mad_1.bff')
+	print(grid)
+	# print(line, grid, '\n', setup, sep = '')
