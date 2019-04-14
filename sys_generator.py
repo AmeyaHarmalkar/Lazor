469e86fd3a2a8529e27403e821d55def39b70c21
HaonanXu <xuhaon@gmail.com>
2019-04-13 23:43:56 -0400

    Initiates systematic approach
    finalizing MC approach for comparsion. And initialize systematic approach

--- sys_generator.py
+++ sys_generator.py
@@ -89,80 +89,82 @@ net_grid = [i for j in grid for i in j]
 print(net_grid)
 
 
-def MC_Generator(net_grid, blocks, steps):
-	'''
-	This is a Monte Carlo based random board generator. We are using a Monte carlo methodology instead of
-	the usual iterative recursive methodology because we believe that Monte carlo with some smart intelligent bias will be faster to parse.
+# def MC_Generator(net_grid, blocks, steps):
+# 	'''
+# 	This is a Monte Carlo based random board generator. We are using a Monte carlo methodology instead of
+# 	the usual iterative recursive methodology because we believe that Monte carlo with some smart intelligent bias will be faster to parse.
 
-	**Parameters**
-		net_grid : *list,str*
-			This is a list of strings stating the nature of the grid. Note that it is not a nested list.
-		blocks : *dict*
-			This is a dict which contains the data about the blocks classified by its type.
-		steps : *int*
-			The steps are the number of Monte Carlo steps you want to perform. Code needs to be rectified a bit to make sure that it 
-			is unrelated to the number of decoy boards in the output.
+# 	**Parameters**
+# 		net_grid : *list,str*
+# 			This is a list of strings stating the nature of the grid. Note that it is not a nested list.
+# 		blocks : *dict*
+# 			This is a dict which contains the data about the blocks classified by its type.
+# 		steps : *int*
+# 			The steps are the number of Monte Carlo steps you want to perform. Code needs to be rectified a bit to make sure that it 
+# 			is unrelated to the number of decoy boards in the output.
 
-	**Returns*
-		ensemble : *list,str*
-			This is a list of strings for now. Modifying the code incorporates running the program and testing it simultaneously so that
-			we don't store anything in memory.
+# 	**Returns*
+# 		ensemble : *list,str*
+# 			This is a list of strings for now. Modifying the code incorporates running the program and testing it simultaneously so that
+# 			we don't store anything in memory.
 
-	'''
+# 	'''
 
 	
-	nA = blocks['A']
-	nC = blocks['C']
-	ensemble = []
+# 	nA = blocks['A']
+# 	nC = blocks['C']
+# 	ensemble = []
 
-	count = 0
+# 	count = 0
 
-	for i in range(0,steps):
+# 	for i in range(0,steps):
 
-		nA = blocks['A']
-		nC = blocks['C']
+# 		nA = blocks['A']
+# 		nC = blocks['C']
 
-		rand_i = random.sample(range(0,len(net_grid)),3)
-		new_grid = []
+# 		rand_i = random.sample(range(0,len(net_grid)),3)
+# 		new_grid = []
 
-		#for i in range(0,steps)
+# 		#for i in range(0,steps)
 
-		for i in range(len(net_grid)):
+# 		for i in range(len(net_grid)):
 
-			if net_grid[i] != 'x':
-				if i in rand_i:
-					if nA > 0 :
-						new_grid.append('A')
-						nA = nA-1
-					elif nA ==0 and nC > 0 :
-						new_grid.append('C')
-						nC = nC-1
-				else:
-					new_grid.append('o')
+# 			if net_grid[i] != 'x':
+# 				if i in rand_i:
+# 					if nA > 0 :
+# 						new_grid.append('A')
+# 						nA = nA-1
+# 					elif nA ==0 and nC > 0 :
+# 						new_grid.append('C')
+# 						nC = nC-1
+# 				else:
+# 					new_grid.append('o')
 
-			elif net_grid[i] == 'x':
-				new_grid.append('x')
+# 			elif net_grid[i] == 'x':
+# 				new_grid.append('x')
 
 		
-			#print(rand_i)
-			#print(new_grid)
+# 			#print(rand_i)
+# 			#print(new_grid)
 
-		ensemble.append(new_grid)
-		#count += 1
+# 		ensemble.append(new_grid)
+# 		#count += 1
 
-	return ensemble
+# 	return ensemble
 
 
 if __name__ == "__main__":
 
-	ans = MC_Generator(net_grid, blocks, 10)
+	pass
 
-	## To get back the grid in the game format from the code
 
-	for i in ans:
-		A = [i[x:x+4] for x in range(0,len(i),4)]
-		print(A)
+	# ans = MC_Generator(net_grid, blocks, 10)
 
+	# ## To get back the grid in the game format from the code
+
+	# for i in ans:
+	# 	A = [i[x:x+4] for x in range(0,len(i),4)]
+	# 	print(A)
 
 
 
